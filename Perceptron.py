24804d4d8d9794368532161a0bb11e6ddd58982b
bilxdi <biladi.amna@mhs.unsoed.ac.id>
2026-05-05 10:13:59 +0700

    buat fungsi menghitung y_in dan fungsi aktivasi

--- Perceptron.py
+++ Perceptron.py
@@ -8,3 +8,11 @@ class Perceptron:
     def __init__(self, alpha=0.1, epoch=10):
         self.alpha = alpha
         self.epoch = epoch
+
+    # Fungsi menghitung nilai y_in atau net
+    def weighted_sum(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+    
+    # Fungsi menerapkan fungsi aktivasi bipolar
+    def predict(self, X):
+        return np.where(self.weighted_sum(X) >= 0.0, 1, -1)
