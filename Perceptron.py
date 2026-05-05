fac21695b460b0505adcf43a5e6a04b8a41657e0
bilxdi <biladi.amna@mhs.unsoed.ac.id>
2026-05-05 10:23:48 +0700

    buat fungsi utama perceptron

--- Perceptron.py
+++ Perceptron.py
@@ -38,3 +38,62 @@ class Perceptron:
         plt.ylabel('X2')
         plt.legend()
         plt.show()
+
+    # Fungsi utama Perceptron
+    def fit(self, X, t):
+        # Inisialisasi bobot dan bias awal = 0
+        self.w_ = np.zeros(1 + X.shape[1])
+
+        # Menyimpan hasil pada HasilPerceptron.txt
+        with open("HasilPerceptron.txt", "w") as f:
+            f.write("Masalah OR dengan Perceptron\n")
+            f.write("----------------------------\n")
+            f.write(f"Input :\n{X}\n")
+            f.write(f"Target:\n{t}\n")
+            f.write(f"Bobot awal : {self.w_[1:]}\n")
+            f.write(f"Bias awal : {self.w_[0]}\n")
+            f.write(f"Learning rate : {self.alpha}\n")
+            f.write(f"Max Epoch : {self.epoch}\n")
+
+            # Iterasi Perceptron(Epoch)
+            for epoch in range(self.epoch):
+                f.write(f"\nEpoch {epoch + 1}/{self.epoch}\n")
+                f.write("----------\n")
+                error = np.array([])
+
+                # Iterasi setiap pasang matriks input dengan targetnya
+                for xi, target in zip(X, t):
+                    # Periksa input dengan model Perceptron
+                    y_pred = self.predict(xi)
+
+                    # Periksa apakah output sesuai target, jika iya maka error = 0
+                    error = np.append(error, target - y_pred)
+
+                    # Modifikasi bobot dengan Delta Rule, jika error = 0 maka bobot tetap
+                    update = self.alpha * error[-1]
+
+                    # Modifikasi bobot w
+                    self.w_[1:] += update * xi
+
+                    # Modifikasi bias b
+                    self.w_[0] += update
+
+                    # Menuliskan hasil tiap iterasi input pada satu epoch
+                    f.write(f"Input: {xi}, Target: {target}, Predict: {y_pred}, Error: {error[-1]}, Bobot: {self.w_[1:]}, Bias: {self.w_[0]}\n")
+
+                # Simulasikan garis pemisah model Perceptron
+                self.plot_decision_boundary(X, t, epoch)
+
+                # Menuliskan penjumlahan kuadrat error setiap input
+                f.write(f"Sum Square Error(SSE): {sum(error ** 2)}\n")
+
+                # Periksa kondisi berhenti, jika penjumlahan kuadrat error setiap input = 0 atau max epoch tercapai
+                if sum(error ** 2) == 0 or epoch + 1 == self.epoch:
+                    f.write("--------------------------------------------------------------------------------\n")
+                    f.write(f"Pelatihan berhenti pada epoch ke-{epoch + 1}karena ")
+                    f.write("Sum Square Error(SSE) mencapai target.\n" if
+                    epoch + 1 != self.epoch else "max epoch tercapai.\n")
+                    # Menuliskan bobot terakhir
+                    f.write(f"\nBobot akhir :{self.w_[1:]}\n")
+                    f.write(f"Bias akhir :{self.w_[0]}")
+                break
